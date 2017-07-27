5ad796a679a54d26d4540bea0a6a51b6b59fb68a
Fabio Carlos <faaceb@gmail.com>
2017-07-26 22:55:40 -0300

    Descobrir diretórios em websites
    Similar ao DirBuster - versão 1.1 - acrescentei um contador para cada diretório analisado.

--- dirFind_1.1.py
+++ dirFind_1.1.py
@@ -10,12 +10,15 @@ import requests
 url = 'http://www.airticketbook.in'
 
 #Arquivo que contem o dicionario de diretorios
-arquivo = 'Dirs1.txt'
+arquivo = 'diretorios.txt'
 with open(arquivo) as arquivoAberto:
     linhas = arquivoAberto.read().splitlines()
 
 #Imprime cabecalho
 print ('URL;STATUS_CODE\n')
+
+#contador
+contador = 0
 
 #Para cada linha no arquivo, execute
 for cadaLinha in linhas:
@@ -24,5 +27,7 @@ for cadaLinha in linhas:
     urlCompleta = url + cadaLinha
     r = requests.head(urlCompleta)
 
+    contador = contador + 1
+
     #Imprime o resultado do acesso
-    print ('%s;%s' % (urlCompleta, r.status_code))
+    print ('%s;%s;%s' % (contador,urlCompleta, r.status_code))

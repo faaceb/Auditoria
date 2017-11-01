5c75a90949e3353c4f5d9e6f82aca0744d454c62
Fabio Carlos <faaceb@gmail.com>
2017-11-01 14:54:32 -0200

    Update varreduraAdminLocal_v1.ps1
    Melhorias na identação

--- varreduraAdminLocal_v1.ps1
+++ varreduraAdminLocal_v1.ps1
@@ -1,9 +1,9 @@
-﻿######################################################################################
+######################################################################################
 # Script em PowerShell para verificar os membros de um grupo local de varias maquinas#
 ######################################################################################
 
 #Obtem a lista de maquinas (hostname)
-$server = Get-Content "C:\TEMP\maquinasInventarioEscopo.txt"
+$server = Get-Content "C:\TEMP\hdu.txt"
 
 #Nome do grupo local a ser pesquisado
 $localgroup = "Administradores"
@@ -16,6 +16,11 @@ foreach ($i in $server){
 	$members = ''
 	$group = [ADSI]"WinNT://$i/$localgroup,group"
     $members = $group.psbase.Invoke("Members")
-    $members | ForEach-Object { $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null) }
+    
+    $members |
+        ForEach-Object {
+            $Nome = $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)
+            Write-Host 'Membro: ' $Nome 
+        }
 
 }
